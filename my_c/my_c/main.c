ae594fdbeeb5a7faef5227c4fc55f44c27432f49
张北辰 <xue_f@zhangbehendembp.lan>
2018-05-28 22:05:25 +0800

    commit 01

---
+++ my_c/my_c/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  my_c
+//
+//  Created by 张北辰 on 2018/5/28.
+//  Copyright © 2018年 xue_f. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
